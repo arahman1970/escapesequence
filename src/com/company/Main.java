5b11af272e00c69d47e7a1b7c908c9adae59f551
arahman1970 <ar.nj.usa@gmail.com>
2019-06-13 14:09:29 -0400

    echo

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+// Write a Java program to print a box, an oval, or a diamond (choose one) using asterisks.
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("*********" + "\n*\t\t*" +"\n*\t\t*" +"\n*\t\t*" +"\n*\t\t*"+"\n*********"  );
+
+        }
+    }
+
